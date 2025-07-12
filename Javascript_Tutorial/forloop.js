053465f3fbd044a8bdc182fb0f1391643cf87ba9
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-12 14:59:33 +0000

    forloop

---
+++ Javascript_Tutorial/forloop.js
@@ -0,0 +1,15 @@
+// for loop and for of in javascript
+
+
+// for loop
+for(let a = 0; a < 11; a++) {
+    console.log(a)
+}
+
+
+// for of
+marks = [80, 11, 45, 67, 56]
+for(let mark in marks) {
+    console.log(mark)
+}
+
