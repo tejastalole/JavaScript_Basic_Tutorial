79b73682eeeceed0032d49561f1ff8aa6d26607f
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-12 15:23:04 +0000

    stringmethods

---
+++ Javascript_Tutorial/stringmethods.js
@@ -0,0 +1,11 @@
+// string methods in javascript
+
+let text = "JavaScript Tutorials For";
+
+console.log(text.charAt(4));    // returns char at postion 4 /index wise/
+console.log(text.concat(" Beginner"));    // adding at the end to the text variable
+console.log(text.endsWith("t"));     // return true when text is end letter is same
+console.log(text.indexOf("v"));   // return the index of v in the text
+console.log(text.replace("For", "Is"));  // replace the text /"searchtext","replacetext"/
+console.log(text.slice(1,4));   // slice the text variable /"startindex", "endindex"/
+
