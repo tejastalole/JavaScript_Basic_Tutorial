22e35153e2a3843fdb629a5c4276038cde19fc1f
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-12 15:31:26 +0000

    arraymethods

--- Javascript_Tutorial/arraymethods.js
+++ Javascript_Tutorial/arraymethods.js
@@ -2,5 +2,5 @@
 
 let color = ["yellow", "black", "white"]
 
-console.log(color.push("pink"));
-console.log(color.join())
+console.log(color.push("pink")); // push methods just added pink color but return the total value 4
+console.log(color.join()) // join methods added the color at the end before we push 
