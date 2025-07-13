6cffc495df40aca1e544b60944ea552af04f867e
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-13 09:04:05 +0000

    windowmethods

---
+++ Javascript_Tutorial/windowmethods.js
@@ -0,0 +1,11 @@
+// window methods in javascript
+
+
+
+alert("Welcome")
+console.log(prompt("Enter Your Name:", "Developer"));
+
+
+// write the above code in html file with script tag 
+// firstly you see the alert box which says you welcome
+// when you ok on this alert box then you see the prompt box  
