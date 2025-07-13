5fd66cf8b813344c82eff4aef9c3e18042c9541b
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-13 10:10:26 +0000

    pr5

---
+++ Javascript_Programs/Program_5.js
@@ -0,0 +1,11 @@
+// JavaScript Program to Check if Number is Positive, Negative, or Zero
+
+let num = -4;
+
+if(num > 0) {
+    console.log(num, "is Positive Number")
+} else if(num < 0) {
+    console.log(num, "is Negative Number")
+} else {
+    console.log(num, "is Zero")
+}
