00aa084e09f23530defc3e62b931c6d253e95ca7
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-13 06:51:38 +0000

    data methods

---
+++ Javascript_Tutorial/datamethods.js
@@ -0,0 +1,26 @@
+// data methods in javascript
+
+
+let current = new Date()
+
+console.log(current.getDate());   // returns current date
+console.log(current.getFullYear());  // returns current full year
+console.log(current.getHours());    // returns current hours
+console.log(current.getMinutes());   // returns current minutes
+console.log(current.getSeconds());  // returns current seconds
+console.log(current.getMonth());   // returns current month
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
