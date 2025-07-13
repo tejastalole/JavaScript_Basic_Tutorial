e5ee49d2d1733c36ea6785a598e4581c49da595c
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-13 06:23:56 +0000

    mathmethods

---
+++ Javascript_Tutorial/mathmethods.js
@@ -0,0 +1,21 @@
+// math methods in javascript
+
+num1 = 16
+num2 = 6
+num3 = 2
+num4 = 3
+num5 = 2.34
+num6 = 12.67
+num7 = -45.6
+
+console.log(Math.sqrt(num1));  // which is the square of num1 = 16 means output 4
+console.log(Math.pow(num3, num4)); // power of num3 to num4 means 2**3 = 8
+console.log(Math.round(num5));  // round of floating point into single integer value
+console.log(Math.ceil(num6));   // ceil get the above round of value
+console.log(Math.floor(num6));  // floor get the below round of value
+console.log(Math.abs(num7));  // it return absolute value means positive
+
+
+
+
+
