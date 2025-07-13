9396efabd67383598da53122900089115dcd57f9
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-13 10:06:52 +0000

    pr4

---
+++ Javascript_Programs/Program_4.js
@@ -0,0 +1,10 @@
+// JavaScript Program to Check if a Number is Odd or Even
+
+
+let num = 3;
+
+if (num % 2 == 0){
+    console.log(num + " is even");
+} else {
+    console.log(num + " is odd");
+}
