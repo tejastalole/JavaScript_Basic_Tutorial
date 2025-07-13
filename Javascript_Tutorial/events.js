e8753e516bef618cafed9393b3454c587b3c5d24
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-13 07:04:31 +0000

    events

---
+++ Javascript_Tutorial/events.js
@@ -0,0 +1,6 @@
+// events in javascript
+
+<button onclick="alert('Button Clicked')">Click Me</button>
+
+// write the above code in html file
+// when you click on button alert message poped out and you see the Button Clicked 
