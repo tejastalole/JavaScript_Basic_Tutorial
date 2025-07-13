f23ad3340df20780500df61f88f6eb1a062c4113
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-13 10:24:19 +0000

    pr6

---
+++ Javascript_Programs/Program_6.js
@@ -0,0 +1,20 @@
+// JavaScript Program to Swap Two Variables
+
+let a = 5;
+let b = 10;
+
+console.log(`Before  Swapping a = ${a}`);
+console.log(`Before Swapping b = ${b}`);
+
+// [b, a] = [a, b];
+// console.log();
+
+temp = a;
+a = b;
+b = temp;
+console.log();
+
+console.log(`After Swapping a = ${a}`);
+console.log(`After Swapping b = ${b}`);
+
+
