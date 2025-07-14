816b02a1dcf9faf4cd77d00b960e4d7f3fb3bdf2
Tejas Talole <143354930+tejastalole@users.noreply.github.com>
2025-07-14 07:51:16 +0000

    pr13

--- Javascript_Programs/Program_13.js
+++ Javascript_Programs/Program_13.js
@@ -1,1 +1,18 @@
-//
+// JavaScript Program to Print Numbers From 1 to n, Replacing Mltiples of 3 With "Fizz", Multiples of 5 With "Buzz", and Multiples of Both 3 and 5 With "FizzBuzz".
+
+
+let arr = [];
+
+for (let n = 0; n < 100; n++) {
+    if (n % 3 === 0 && n % 5 === 0) {
+        arr.push("FizzBuzz");
+    } else if (n % 3 === 0) {
+        arr.push("Fizz");
+    } else if (n % 5 === 0) {
+        arr.push("Buzz");
+    } else {
+        arr.push(n);
+    }
+}
+
+console.log(arr)
